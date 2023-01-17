446da9875e8385e74bec4fa1c1f39e3ba68c5d23
Patrice-H <webmestre64@gmail.com>
2023-01-17 14:45:56 +0100

    Init component properties

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss'],
 })
-export class FaceSnapComponent {}
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createdDate!: Date;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit() {
+    this.title = 'Blender';
+    this.description = 'Mon logiciel 3D préféré';
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imageUrl = '../../assets/blender.png';
+  }
+}
