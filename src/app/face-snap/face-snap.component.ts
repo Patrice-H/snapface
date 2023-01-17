e2ae6c8ea682d1a7920d72a05391e1d3e1677eb5
Patrice-H <webmestre64@gmail.com>
2023-01-17 17:57:11 +0100

    Add onLike method and properties

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -9,14 +9,26 @@ export class FaceSnapComponent implements OnInit {
   title!: string;
   description!: string;
   createdDate!: Date;
-  snaps!: number;
+  likes!: number;
   imageUrl!: string;
+  componentIsLiked!: boolean;
+  likeBtnText!: string;
 
   ngOnInit() {
     this.title = 'Blender';
     this.description = 'Mon logiciel 3D préféré';
     this.createdDate = new Date();
-    this.snaps = 6;
+    this.likes = 6;
     this.imageUrl = '../../assets/blender.png';
+    this.componentIsLiked = false;
+    this.likeBtnText = 'Ajouter un like';
+  }
+
+  onLike() {
+    if (!this.componentIsLiked) {
+      this.likes++;
+      this.likeBtnText = 'Vous avez déjà ajouté un like !';
+      this.componentIsLiked = true;
+    }
   }
 }
