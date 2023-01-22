7d9c990c6006e3228fa93802adfbcb106478025d
Patrice-H <webmestre64@gmail.com>
2023-01-22 18:44:11 +0100

    Add Gimp card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,25 +14,33 @@ export class AppComponent implements OnInit {
       {
         title: 'Blender',
         description: 'Logiciel de modélisation 3D',
-        createdDate: new Date,
+        createdDate: new Date(),
         likes: 3,
         imageUrl: '../../assets/blender.png',
-        openSourceUrl: '../../assets/opensource.png'
+        openSourceUrl: '../../assets/opensource.png',
       },
       {
         title: 'Photoshop',
         description: 'Logiciel de retouche photo',
-        createdDate: new Date,
+        createdDate: new Date(),
         likes: 2,
-        imageUrl: '../../assets/photoshop.png'
+        imageUrl: '../../assets/photoshop.png',
       },
       {
         title: 'Illustrator',
         description: 'Logiciel de dessin',
-        createdDate: new Date,
+        createdDate: new Date(),
         likes: 1,
-        imageUrl: '../../assets/illustrator.png'
-      }
+        imageUrl: '../../assets/illustrator.png',
+      },
+      {
+        title: 'Gimp',
+        description: "Logiciel éditeur d'images multiplateforme",
+        createdDate: new Date(),
+        likes: 2,
+        imageUrl: '../../assets/gimp.png',
+        openSourceUrl: '../../assets/opensource.png',
+      },
     ];
   }
 }
