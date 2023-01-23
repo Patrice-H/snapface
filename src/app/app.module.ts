8c1cc3d1cc599635272b28d255cc36526ff3b8cf
Patrice-H <webmestre64@gmail.com>
2023-01-23 18:55:08 +0100

    Add a module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { registerLocaleData } from '@angular/common';
 import * as fr from '@angular/common/locales/fr';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 
 @NgModule({
-  declarations: [AppComponent, FaceSnapComponent],
+  declarations: [AppComponent, FaceSnapComponent, FaceSnapListComponent],
   imports: [BrowserModule],
   providers: [{ provide: LOCALE_ID, useValue: 'fr-FR' }],
   bootstrap: [AppComponent],
